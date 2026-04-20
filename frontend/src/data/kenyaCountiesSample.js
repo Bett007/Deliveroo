57f1d9b3670af3f661fcf83d22b853aed8851ad5
Bett007 <bettbrian022@gmail.com>
2026-04-20 15:06:13 +0300

    Add missing kenya counties sample data for route map build

---
+++ frontend/src/data/kenyaCountiesSample.js
@@ -0,0 +1,103 @@
+export const kenyaCountiesSampleGeoJson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: { name: "Nairobi", code: "047" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [36.63, -1.45],
+          [37.10, -1.45],
+          [37.10, -1.12],
+          [36.63, -1.12],
+          [36.63, -1.45],
+        ]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Kiambu", code: "022" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [36.52, -1.30],
+          [37.28, -1.30],
+          [37.28, -0.68],
+          [36.52, -0.68],
+          [36.52, -1.30],
+        ]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Machakos", code: "016" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [36.55, -1.92],
+          [37.82, -1.92],
+          [37.82, -1.02],
+          [36.55, -1.02],
+          [36.55, -1.92],
+        ]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Kisumu", code: "042" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [34.38, -0.30],
+          [35.02, -0.30],
+          [35.02, 0.30],
+          [34.38, 0.30],
+          [34.38, -0.30],
+        ]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Nakuru", code: "032" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [35.64, -1.05],
+          [36.62, -1.05],
+          [36.62, 0.35],
+          [35.64, 0.35],
+          [35.64, -1.05],
+        ]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Uasin Gishu", code: "027" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [35.02, 0.00],
+          [35.72, 0.00],
+          [35.72, 0.76],
+          [35.02, 0.76],
+          [35.02, 0.00],
+        ]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { name: "Mombasa", code: "001" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[
+          [39.50, -4.25],
+          [39.80, -4.25],
+          [39.80, -3.90],
+          [39.50, -3.90],
+          [39.50, -4.25],
+        ]],
+      },
+    },
+  ],
+};
